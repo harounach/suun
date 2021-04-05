7956fafe6df0ec8977c5539160d1eecfb7c2f322
Haroun Ach <harounacho21@gmail.com>
2021-04-05 17:17:12 +0100

    Fetch data from api

--- api/weatherRoute.js
+++ api/weatherRoute.js
@@ -2,8 +2,34 @@ require("dotenv").config();
 const { Router } = require("express");
 const router = Router();
 
+// axios http client
+const axios = require("axios").default;
+// 48.8566° N, 2.3522° E
+const Paris = {
+  lon: 2.3522,
+  lat: 48.8566,
+};
+
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+const OPEN_WEATHER_MAP = "https://api.openweathermap.org/data/2.5/onecall";
+
+// testing endpoint
 router.route("/").get((req, res) => {
   res.json({ message: "It works!" });
+});
+
+router.route("/").post(async (req, res) => {
+  const { search } = req.body;
+  const response = await axios(OPEN_WEATHER_MAP, {
+    params: {
+      lat: Paris.lat,
+      lon: Paris.lon,
+      exclude: "current,minutely,hourly,alerts",
+      appid: process.env.API_KEY,
+    },
+  });
+  console.log(response.data);
+  res.json(response.data);
 });
 
 module.exports = router;
