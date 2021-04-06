182b3a1153879619f1d6afe0f08c0e65bcb2a3b1
Haroun Ach <harounacho21@gmail.com>
2021-04-06 14:04:43 +0100

    Populate days with data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,16 @@ window.addEventListener("DOMContentLoaded", () => {
    *  @typedef {Object} WeatherItem
    *
    * @property {number} dt - Date time
+   * @property {Temperature} temp - Temperature
+   * @property {Array<WeatherCondition>} weather - Weather conditions
+   *
+   */
+
+  /**
+   * Current Weather Item
+   *  @typedef {Object} CurrentWeatherItem
+   *
+   * @property {number} dt - Date time
    * @property {number} temp - Temperature
    * @property {Array<WeatherCondition>} weather - Weather conditions
    *
@@ -18,6 +28,18 @@ window.addEventListener("DOMContentLoaded", () => {
    * @property {number} id - Weather condition id
    * @property {string} main - Weather condition name
    * @property {string} description - Weather condition description
+   */
+
+  /**
+   * Weather Temperature
+   * @typedef {Object} Temperature
+   *
+   * @property {number} day
+   * @property {number} eve
+   * @property {number} max
+   * @property {number} min
+   * @property {number} morn
+   * @property {number} night
    */
 
   // Model
@@ -70,8 +92,8 @@ window.addEventListener("DOMContentLoaded", () => {
       return model.imagesMap[main];
     },
 
-    formatDegrees: function (degree) {
-      return Math.round(degree);
+    formatTemperature: function (temp) {
+      return Math.round(temp);
     },
   };
 
@@ -86,38 +108,60 @@ window.addEventListener("DOMContentLoaded", () => {
     },
 
     render: function (weatherData) {
-      console.log(weatherData);
+      // console.log(weatherData);
 
       const current = weatherData["current"];
       const daily = weatherData["daily"];
       this.populateToday(current);
-      this.populateDays(daily);
-
-      // populate days
-      // this.days.forEach((day) => {
-      //   day.getElementsByClassName("day__name")[0].textContent = 55;
-      // });
+      this.populateAllDays(daily);
     },
     showError: function () {},
+
+    /**
+     * @param {CurrentWeatherItem} current
+     */
     populateToday: function (current) {
-      const main = current["weather"][0]["main"];
+      const main = current.weather[0].main;
       const imageUrl = controller.chooseImage(main);
       this.todayImage.src = imageUrl;
 
-      const formattedDegree = controller.formatDegrees(current["temp"]);
-      this.todayDegrees.textContent = `${formattedDegree}°`;
+      const formattedTemperature = controller.formatTemperature(current.temp);
+      this.todayDegrees.textContent = `${formattedTemperature}°`;
     },
 
     /**
      * @param {WeatherItem} currentDay
      * @param {HTMLDivElement} day
      */
-    populateDay: function (currentDay, day) {},
+    populateDay: function (currentDay, day) {
+      console.log(currentDay);
+      // Get temperature
+      const temperature = controller.formatTemperature(currentDay.temp.day);
+      // Get image
+      const main = currentDay.weather[0].main;
+      const imagesUrl = controller.chooseImage(main);
+
+      console.log(imagesUrl);
+      console.log(day);
+
+      // Set temperature
+      day.querySelector(".day__degrees").textContent = temperature;
+      // Set image
+      day.querySelector(".day__img").src = imagesUrl;
+    },
 
     /**
-     * @param {Array<DayWeather>} daily
+     * @param {Array<WeatherItem>} daily
      */
-    populateDays: function (daily) {},
+    populateAllDays: function (daily) {
+      daily.forEach((weatherItem) => {});
+
+      // populate days
+      this.days.forEach((day, index) => {
+        // day.getElementsByClassName("day__name")[0].textContent = 55;
+        this.populateDay(daily[index], day);
+      });
+    },
   };
 
   // init App
